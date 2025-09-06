15289adad8b6432ef44b0294dceab7b57fe2181d
raviverma <vermaraghav36@gmail.com>
2025-09-06 15:39:55 +0200

    added shuffle_mode

--- main_settings.py
+++ main_settings.py
@@ -4,12 +4,16 @@ pygame.init()
 # Initialize the mixer
 pygame.mixer.init()
 class Settings:
+
+    # Quiz Settings
     trials=2 #1 being the lowest
     show_article=False
-    volume_limit=1.0  #0-1
+    shuffle_mode=True
+
+    # Sound & Audio
+    volume_limit=1.0  #0.0-1.0
     sound_enable=True
     sound_correct = pygame.mixer.Sound("correct-156911.mp3")
     sound_wrong = pygame.mixer.Sound("error-010-206498.mp3")
-    shuffle_mode=False
 
 
